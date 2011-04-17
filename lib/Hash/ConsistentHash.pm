9571cf2560bf3978df3ec5a399862c4d297e0c59
Luben Karavelov <karavelov@spnet.net>
2011-04-17 19:17:13 +0000

    Import of KARAVELOV/Hash-ConsistentHash-0.03 from CPAN.
    gitpan-cpan-distribution: Hash-ConsistentHash
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         KARAVELOV/Hash-ConsistentHash-0.03.tar.gz
    gitpan-cpan-author:       KARAVELOV
    gitpan-cpan-maturity:     released

---
+++ lib/Hash/ConsistentHash.pm
@@ -0,0 +1,221 @@
+package Hash::ConsistentHash;
+
+use v5.10;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Hash::ConsistentHash - Constant hash algorithm
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.03';
+
+
+=head1 SYNOPSIS
+
+    use Hash::ConsistentHash;
+    use String::CRC32;
+
+    my $chash = Hash::ConsistentHash->new(
+        buckets   => [qw(10.0.0.1 10.0.0.2 10.0.0.3 10.0.0.4)],
+        hash_func => \&crc32
+    );
+    my $next  = $chash->lookup('foo');
+    my $server= $next->(); # get bucket
+    # do stuff with $server
+    $server   = $next->(); # get another bucket
+    ...
+
+=head1 DESCRIPTION
+
+Hash::ConsistentHash algorithm distributes keys over fixed number of buckets. 
+Constant hash distribution means that if we add a bucket to a hash with N 
+buckets filled with M keys we have to reassign only M/(N+1) keys to new 
+buckets.
+
+=head1 METHODS
+
+=head2 new
+
+Creates ConsistentHash object. It accept following params:
+
+=over
+
+=item buckets
+
+Arrayref or Hashref. If buckets are given as arrayref they will have
+same weight. If given as hashref, every bucket could have differend
+weight. 
+
+Examples:
+
+    # All buckets have same weight so they will hold equal amount of keys
+    my $chash = Hash::ConsistentHash->new( 
+        buckets => [qw(A B C)], 
+        hash_func=>\&crc32 );
+
+    # Bucket "B" will hold twice the amount of keys of bucket A or C
+    my $chash = Cash::ConsistentHash->new( 
+        buckets => {A=>1, B=>2, C=>1},
+        hash_func=>\&crc32 );
+
+
+=item factor
+
+Randomization factor for the sequence. Default 10.
+
+=back
+
+=cut
+
+sub new {
+    my $self   = shift;
+    my $class  = ref($self)||$self;
+    $self   = bless {bukets=>0}, $class;
+
+    my %params = @_;
+    my $max    = $params{factor} // 10;
+    die "You showld specify hash_func coderef" 
+        unless ref($params{hash_func}) eq 'CODE';
+    $self->{hash_func} = $params{hash_func};
+    my (@dest,$weight);
+    if (ref $params{buckets} eq 'ARRAY'){
+        @dest  = @{$params{buckets}};
+        $weight= { map {$_ => 1 } @dest };
+    }elsif(ref $params{buckets} eq 'HASH'){
+        @dest  = keys %{$params{buckets}};
+        $weight= $params{buckets};
+    }
+    return unless @dest;
+    $self->{buckets} = scalar(@dest);
+    $self->{ring} = {
+        map{
+            srand($self->{hash_func}->(my $dest = $_));
+            my %result;
+            $result{int(rand(0xFFFFFFFF))}=$dest
+                for ( 1..$max * $weight->{$dest} );
+            %result
+        } @dest };
+    $self->{sorted} = [ sort {$a <=> $b } keys ( %{$self->{ring}} ) ];
+    return $self;
+}
+
+
+=head2 lookup
+
+Lookup a key in the hash. Accept one param - the key. Returns an iterator 
+over the hash buckets.
+
+Example: 
+    my $chash = Hash::ConsistentHash->new( 
+        buckets => [qw(A B C)], 
+        hash_func=>\&crc32 );
+
+    my $next   = $chash->lookup('foo');
+    my $bucket = $next->(); # B
+    $bucket    = $next->(); # A 
+    $bucket    = $next->(); # C, hash is exhausted 
+    $bucket    = $next->(); # A 
+    ...
+
+Returned buckets will not repeat until all buckets are exhausted.
+
+=cut
+
+sub lookup {
+    my ($self,$key) = @_;
+    my $idx = $self->{hash_func}->($key);
+    my $ring= $self->{ring};
+    my %seen;
+    my $returned = 0;
+    return sub {
+        my $first;
+        # start from the beggining if we have already returned all buckets
+        if ($returned >= $self->{buckets}){
+            $returned = 0;
+            %seen = ();
+        }
+        while (1){
+            for my $n ( @{ $self->{sorted} }){
+                $first //= $n;
+                if(($n > $idx) and (not $seen{$ring->{$n}})){
+                    $seen{$ring->{$n}} = 1;
+                    $returned ++;
+                    return $ring->{$idx = $n};
+                }
+            }
+            $idx = $first;
+            if (not $seen{$ring->{$idx}}){
+                $seen{$ring->{$idx}} = 1;
+                $returned ++;
+                return $ring->{$idx};
+            }
+        }
+    }
+}
+
+=head1 AUTHOR
+
+Luben Karavelov, C<< <karavelov at spnet.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-hash-consistenthash at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Hash-ConsistentHash>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Hash::ConsistentHash
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GIT repository with the latest stuff
+
+L<https://github.com/luben/Hash-ConsistentHash>
+L<git://github.com/luben/Hash-ConsistentHash.git>
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Hash-ConsistentHash>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Hash-ConsistentHash>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Hash-ConsistentHash>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Hash-ConsistentHash/>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Luben Karavelov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Hash::ConsistentHash
